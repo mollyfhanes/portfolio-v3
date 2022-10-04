85960326ca650b0931027038598fb75d4838a496
Molly Hanes <mollyfhanes@gmail.com>
2022-10-04 13:36:34 -0500

    got linter working

--- src/App.js
+++ src/App.js
@@ -7,21 +7,28 @@ import Tabs from '@mui/material/Tabs'
 import Tab from '@mui/material/Tab'
 import avatar from './avatar.png'
 import Home from './Home'
-import Skills from './Skills';
+import Skills from './Skills'
 
 function App() {
   const [value, setValue] = React.useState(0)
   const handleChange = (e, newValue) => setValue(newValue)
 
   return (
-    <Box sx={{ width: '100%', paddingLeft: '5vw', paddingRight: '5vw'}}>
+    <Box sx={{ width: '100%', paddingLeft: '5vw', paddingRight: '5vw' }}>
       <Tabs variant="fullWidth" value={value} onChange={handleChange}>
         <Tab label="Home" />
         <Tab label="Skills" />
         <Tab label="About" />
         <Tab label="Contact" />
       </Tabs>
-      <Grid container spacing={5} xs display="flex" justifyContent="center" alignItems="center">
+      <Grid
+        container
+        spacing={5}
+        xs
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
         {value === 0 && <Home />}
         {value === 1 && <Skills />}
       </Grid>
